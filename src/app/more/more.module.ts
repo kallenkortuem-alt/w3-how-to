7382e033915fcf16350dacb9ae5a749d496cb3c0
Kallen Kortuem <kallenko@magenic>
2017-12-11 11:45:21 -0600

    added basic card

--- src/app/more/more.module.ts
+++ src/app/more/more.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactCardComponent } from './contact-card/contact-card.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        ContactCardComponent
+        ContactCardComponent,
+        CardComponent
     ],
     exports: [
-        ContactCardComponent
+        ContactCardComponent,
+        CardComponent
     ]
 })
 export class MoreModule { }
